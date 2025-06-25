63595e64a9834ca4cf9157b7ab14413a23ae5cc9
kgrover3 <kenneth.grover@snhu.edu>
2025-06-24 22:12:02 -0400

    Changed build path as dll was not loading from within Harmony directory. fixed ElementalSurvivalMain.cs having two extensions.

--- src/Elemental_Survival/Elemental_Survival/ElementalSurvivalMain.cs
+++ src/Elemental_Survival/Elemental_Survival/ElementalSurvivalMain.cs
@@ -6,7 +6,7 @@ public class ElementalSurvivalMain : IModApi
     public void InitMod(Mod mod)
     {
         Log.Out("[ElementalSurvival] Initializing mod and applying Harmony patches...");
-        var harmony = new Harmony("com.yourname.elementalsurvival");
+        var harmony = new Harmony("com.rygorn85.elementalsurvival");
         harmony.PatchAll();
     }
 }
